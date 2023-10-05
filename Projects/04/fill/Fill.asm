20cdbbfbe6c7fabc5c56042e9658f9e4ef0c29b8
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-10-05 03:57:50 +0300

    Added week 4 projects

--- Projects/04/fill/Fill.asm
+++ Projects/04/fill/Fill.asm
@@ -1,7 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
 
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
@@ -11,4 +7,51 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(BEGIN)
+    // Set first to first screen pixel map.
+    @16384
+    D=A
+    @first
+    M=D
+    // Set current to last screen pixel map.
+	@24575
+	D=A
+	@current
+	M=D
+    // Check the keyboar input.
+    @KBD
+    D=M
+    // If keyboard is pressed, fill the screen.
+    @BLACK
+    D;JNE
+    // else, clear the screen.
+    @CLEAR
+    D;JEQ
+    (BLACK)
+        @fillvalue
+        M=-1
+        @DRAW
+        0;JMP
+    (CLEAR)
+        @fillvalue
+        M=0
+(DRAW)
+    // Fill or clear current pixel, depending on fillvalue.
+    @fillvalue
+    D=M
+    @current
+    A=M
+    M=D
+    // If current pixel map is the first pixel map there is nothing left to draw, so jump back to the begin.
+    @current
+	D=M
+    @first
+    D=D-M
+    @BEGIN
+    D;JEQ
+    // Decrement current pixel map.
+    @current
+    M=M-1
+    // continue drawing pixel map.
+    @DRAW
+    0;JMP
--- Projects/04/mult/Mult.asm
+++ Projects/04/mult/Mult.asm
@@ -1,12 +1,63 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 //
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+(BEGIN)
+    // sets the maximum value
+    @Max
+    M=-1
+    // clear R2 value
+    @R2
+    M=0
+    // if R0 >= 0 continue the program, else end the program
+    @R0
+    D=M
+    @END // goto END
+    D;JLT
+    // if R1 >= 0 continue the program, else end the program
+    @R1
+    D=M
+    @END // goto END
+    D;JLE
+    // loop iterator
+    @i
+    M=0
 
-// Put your code here.
+(LOOP)
+    // if i = R1 end the program, else complete the program
+    @i 
+    D=M
+    @R1
+    D=D-M
+    @END // goto END
+    D;JEQ
+    // R2 = R2+R0
+    @R0
+    D=M
+    @R2
+    M=M+D
+    // i = i + 1
+    @i
+    M=M+1
+    // if R0*R1 exceeded the limit, else continue the program
+    @Max
+    D=M
+    @R2
+    // R0*R1 - Max
+    D=D-M 
+    @STOP // goto end
+    D;JGE 
+    @LOOP // goto Loop
+    0;JMP
+
+(STOP) 
+    // clear R2 register
+    @R2
+    M=0
+
+    // infinite loop
+(END)
+    @END
+    0;JMP
